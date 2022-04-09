a5c82168dde78da65d8909ff726c93202b60e099
Aidan Chamberlain <asunshine99@gmail.com>
2022-04-09 14:52:09 -0500

    added comment

--- models/index.js
+++ models/index.js
@@ -2,4 +2,5 @@ const Student = require("./Student");
 const Course = require("./Course");
 const User = require("./User");
 
+// TODO, REMOVE STUDENT & COURSE
 module.exports = { Student, Course, User };
