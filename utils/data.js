ec424bbeffba3cbeb699c048f3fa5f74c13954e8
Aidan Chamberlain <asunshine99@gmail.com>
2022-04-19 22:56:44 -0500

    updated seed.js, seed data

--- utils/data.js
+++ utils/data.js
@@ -1,118 +1,46 @@
-const names = [
-  'Aaran',
-  'Aaren',
-  'Aarez',
-  'Aarman',
-  'Aaron',
-  'Aaron-James',
-  'Aarron',
-  'Aaryan',
-  'Aaryn',
-  'Aayan',
-  'Aazaan',
-  'Abaan',
-  'Abbas',
-  'Abdallah',
-  'Abdalroof',
-  'Abdihakim',
-  'Abdirahman',
-  'Abdisalam',
-  'Abdul',
-  'Abdul-Aziz',
-  'Abdulbasir',
-  'Abdulkadir',
-  'Abdulkarem',
-  'Smith',
-  'Jones',
-  'Coollastname',
-  'enter_name_here',
-  'Ze',
-  'Zechariah',
-  'Zeek',
-  'Zeeshan',
-  'Zeid',
-  'Zein',
-  'Zen',
-  'Zendel',
-  'Zenith',
-  'Zennon',
-  'Zeph',
-  'Zerah',
-  'Zhen',
-  'Zhi',
-  'Zhong',
-  'Zhuo',
-  'Zi',
-  'Zidane',
-  'Zijie',
-  'Zinedine',
-  'Zion',
-  'Zishan',
-  'Ziya',
-  'Ziyaan',
-  'Zohaib',
-  'Zohair',
-  'Zoubaeir',
-  'Zubair',
-  'Zubayr',
-  'Zuriel',
-  'Xander',
-  'Jared',
-  'Courtney',
-  'Gillian',
-  'Clark',
-  'Jared',
-  'Grace',
-  'Kelsey',
-  'Tamar',
-  'Alex',
-  'Mark',
-  'Tamar',
-  'Farish',
-  'Sarah',
-  'Nathaniel',
-  'Parker',
+const users = [
+    {
+        username: "achamberlain",
+        email: "achamberlain12@gmail.com",
+    },
+    {
+        username: "mgriffin",
+        email: "mgriffin23@gmail.com",
+    },
+    {
+        username: "mpalmer",
+        email: "mpalmer34@gmail.com",
+    },
+    {
+        username: "scernera",
+        email: "scernera45@gmail.com",
+    },
+    {
+        username: "jmilner",
+        email: "jmilner56@gmail.com",
+    },
 ];
 
-const appDescriptions = [
-  'Decision Tracker',
-  'Find My Phone',
-  'Learn Piano',
-  'Starbase Defender',
-  'Tower Defense',
-  'Monopoly Money Manager',
-  'Movie trailers',
-  'Hello world',
-  'Stupid Social Media App',
-  'Notes',
-  'Messages',
-  'Email',
-  'Compass',
-  'Firefox',
-  'Running app',
-  'Cooking app',
-  'Poker',
-  'Deliveries',
-];
+// const thoughts = ["The Thing is the best movie", "Meshuggah is the best band", "Gyros are the best food", "Lou Malnati's is the best deep dish"];
 
 // Get a random item given an array
-const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+// const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-// Gets a random full name
-const getRandomName = () =>
-  `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
+// // Gets a random full name
+// const getRandomName = () => `${getRandomArrItem(users)}}`;
 
-// Function to generate random assignments that we can add to student object.
-const getRandomAssignments = (int) => {
-  const results = [];
-  for (let i = 0; i < int; i++) {
-    results.push({
-      assignmentName: getRandomArrItem(appDescriptions),
-      score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
-    });
-  }
-  return results;
-};
+// // Function to generate random assignments that we can add to student object.
+// const getRandomAssignments = (int) => {
+//     const results = [];
+//     for (let i = 0; i < int; i++) {
+//         results.push({
+//             assignmentName: getRandomArrItem(thoughts),
+//             score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
+//         });
+//     }
+//     return results;
+// };
 
 // Export the functions for use in seed.js
-module.exports = { getRandomName, getRandomAssignments };
+// module.exports = { getRandomName, getRandomAssignments };
+module.exports = { users };
--- utils/seed.js
+++ utils/seed.js
@@ -1,6 +1,8 @@
 const connection = require("../config/connection");
-const { Course, Student, User } = require("../models");
-const { getRandomName, getRandomAssignments } = require("./data");
+// const { Course, Student, User } = require("../models");
+const { User } = require("../models");
+// const { getRandomName, getRandomAssignments } = require("./data");
+const { users } = require("./data");
 
 connection.on("error", (err) => err);
 
@@ -8,51 +10,49 @@ connection.once("open", async () => {
     console.log("connected");
 
     // Drop existing courses
-    await Course.deleteMany({});
+    // await Course.deleteMany({});
 
     // Drop existing students
-    await Student.deleteMany({});
+    // await Student.deleteMany({});
+
+    await User.deleteMany({});
 
     // Create empty array to hold the students
-    const students = [];
-    const users = [
-        {
-            username: "dblaine",
-            email: "dblaine@gmail.com",
-        },
-    ];
+    // const users = [
+    //     {
+    //         username: "dblaine",
+    //         email: "dblaine@gmail.com",
+    //     },
+    // ];
 
     // Loop 20 times -- add students to the students array
-    for (let i = 0; i < 20; i++) {
-        // Get some random assignment objects using a helper function that we imported from ./data
-        const assignments = getRandomAssignments(20);
+    // for (let i = 0; i < users.length; i++) {
+    //     // Get some random assignment objects using a helper function that we imported from ./data
+    //     // const email = getRandomAssignments(20);
+    //     // const username = getRandomName();
+    //     // const first = fullName.split(" ")[0];
+    //     // const last = fullName.split(" ")[1];
+    //     // const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
 
-        const fullName = getRandomName();
-        const first = fullName.split(" ")[0];
-        const last = fullName.split(" ")[1];
-        const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
-
-        students.push({
-            first,
-            last,
-            github,
-            assignments,
-        });
-    }
+    //     users.push({
+    //         username,
+    //         email,
+    //     });
+    // }
 
     // Add students to the collection and await the results
-    await Student.collection.insertMany(students);
+    // await Student.collection.insertMany(students);
     await User.collection.insertMany(users);
 
     // Add courses to the collection and await the results
-    await Course.collection.insertOne({
-        courseName: "UCLA",
-        inPerson: false,
-        students: [...students],
-    });
+    // await Course.collection.insertOne({
+    //     courseName: "UCLA",
+    //     inPerson: false,
+    //     students: [...students],
+    // });
 
     // Log out the seed data to indicate what should appear in the database
-    console.table(students);
-    console.info("Seeding complete! 🌱");
+    console.table(users);
+    console.info("Seeding successful!");
     process.exit(0);
 });
